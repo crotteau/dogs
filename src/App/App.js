55897a66d9c308c266f8c3c45d8db1bfb49c4420
Megan Crotteau <megan.crotteau@gmail.com>
2024-06-11 15:05:09 -0600

    Dropdown menu options

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,21 @@
 
 import './App.css';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import BreedSelector from '../BreedSelector/BreedSelector';
 
 function App() {
-  
+
   return (
     <div className="App">
       <header className="App-header">
-      <h2>Dogs!</h2>
+        <h2>Fetch!</h2>
       </header>
-
+      <BreedSelector />
+      {/* <Routes> */}
+        {/* <Route path="/" element={<Breeds />}></Route>
+        <Route path="/:breed" element={<BreedImages />}></Route>
+        <Route path="/:breed/:subBreeds" element={<BreedImages />}></Route> */}
+      {/* </Routes> */}
     </div>
   );
 }
--- src/BreedSelector/BreedSelector.js
+++ src/BreedSelector/BreedSelector.js
@@ -1,10 +1,57 @@
-import '/BreedSelector.css';
+import './BreedSelector.css'
 import { getAllBreeds } from '../apiCalls';
+import { useEffect, useState } from 'react';
+import Select from 'react-select';
 
 function BreedSelector() {
+    const [breedOptions, setOptions] = useState([])
+    const [breedObjects, setBreedObjects] = useState([])
+    const [selectionOption, setSelectedBreed] = useState(null)
 
+    useEffect(() => {
+        getBreeds()
+    }, [])
 
+    useEffect(() => {
+        formatOptions()
+    }, [breedOptions])
 
+    const getBreeds = async () => {
+        try {
+            const breeds = await getAllBreeds()
+            if (breeds) {
+                console.log('breeds', breeds.message)
+                setOptions(breeds.message)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const formatOptions = () => {
+        Object.keys(breedOptions).map(breed => {
+            breedObjects.push({value: breed, label: breed})
+        })
+    }
+
+    return (
+        <div className='select'>
+            <Select
+                defaultValue={selectionOption}
+                onChange={setSelectedBreed}
+                options={breedObjects}
+                isMulti
+                styles={{
+                    control: (baseStyles, state) => ({
+                        ...baseStyles,
+                        width: '20vw',
+                        borderColor: state.isFocused ? 'blue' : 'grey',
+                        cursor: 'pointer'
+                    })
+                }}
+            />
+        </div>
+    )
 }
 
 export default BreedSelector;
